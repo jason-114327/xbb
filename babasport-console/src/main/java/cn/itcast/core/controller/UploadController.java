63d52970550e7ddf03530583e319cfded0c6dcaa
JASON\57255 <572556363@qq.com>
2020-08-22 15:05:20 +0800

    商品管理-》上传多张图片

--- babasport-console/src/main/java/cn/itcast/core/controller/UploadController.java
+++ babasport-console/src/main/java/cn/itcast/core/controller/UploadController.java
@@ -1,6 +1,8 @@
 package cn.itcast.core.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -9,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import cn.itcast.common.web.Constants;
@@ -17,29 +20,46 @@ import cn.itcast.core.service.upload.UploadService;
 
 /**
  * 上传图片
- * @author lx
  *
+ * @author lx
  */
 @Controller
 public class UploadController {
 
-	@Autowired
-	private UploadService uploadService;
-	//上传图片
-	@RequestMapping(value = "/upload/uploadPic.do")
-	public void uploadPic(@RequestParam(required = false) MultipartFile pic
-			,HttpServletResponse response) throws IOException{
-		System.out.println(pic);
-		
-		String path = uploadService.uploadPic(pic.getBytes(), pic.getOriginalFilename(), pic.getSize());
-		
-		String url = Constants.IMG_URL + path;
-		
-		JSONObject  jo = new JSONObject();
-		jo.put("url", url);
-		 
-		response.setContentType("application/json;charset=UTF-8");
-		response.getWriter().write(jo.toString());
-		
-	}
+    @Autowired
+    private UploadService uploadService;
+
+    //上传图片
+    @RequestMapping(value = "/upload/uploadPic.do")
+    public void uploadPic(@RequestParam(required = false) MultipartFile pic
+            , HttpServletResponse response) throws IOException {
+        System.out.println(pic);
+
+        String path = uploadService.uploadPic(pic.getBytes(), pic.getOriginalFilename(), pic.getSize());
+
+        String url = Constants.IMG_URL + path;
+
+        JSONObject jo = new JSONObject();
+        jo.put("url", url);
+
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().write(jo.toString());
+    }
+
+    //上传多张图片
+    @RequestMapping(value = "/upload/uploadPics.do")
+    @ResponseBody
+    public List<String> uploadPics(@RequestParam(required = false) MultipartFile[] pics
+            , HttpServletResponse response) throws IOException {
+        System.out.println(pics);
+
+        List<String> urls = new ArrayList<String>();
+        for (MultipartFile pic : pics) {
+            String path = uploadService.uploadPic(pic.getBytes(), pic.getOriginalFilename(), pic.getSize());
+            String url = Constants.IMG_URL + path;
+            urls.add(url);
+        }
+
+      return urls;
+    }
 }
