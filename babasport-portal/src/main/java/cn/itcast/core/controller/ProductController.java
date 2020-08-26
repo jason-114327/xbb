c5c9023af19279fd1817ebe5145cb0d5d5b9f932
JASON\57255 <572556363@qq.com>
2020-08-26 11:40:29 +0800

    搭建portal前台系统

---
+++ babasport-portal/src/main/java/cn/itcast/core/controller/ProductController.java
@@ -0,0 +1,17 @@
+package cn.itcast.core.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 前台商品
+ */
+@Controller
+public class ProductController {
+
+    // 去首页 入口
+    @RequestMapping(value = "/")
+    public String index(){
+        return "index";
+    }
+}
